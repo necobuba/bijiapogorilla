fad845a4df1a80f2d94da663e74c73d2f0dee70c
Glitch (pesmerga) <none>
2022-03-01 16:34:07 +0000

    🔊🔀 Updated with Glitch

---
+++ rudawtest.js
@@ -0,0 +1,8 @@
+function play_stream(url) {
+    var video = document.getElementById('prank__first');
+    var m3u8Url = decodeURIComponent(url);
+  
+return m3u8Url 
+}
+
+play_stream("https://svs.itworkscdn.net/rudawlive/rudawlive.smil/rudawtv_720p_chunks.m3u8")
